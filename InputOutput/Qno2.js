10f881dd797565279470e4fbf720e50af79e3037
Muthu <84056393+mkandanmk7@users.noreply.github.com>
2021-07-31 21:50:35 +0530

    seperate by space.

---
+++ InputOutput/Qno2.js
@@ -0,0 +1,23 @@
+//Print the integer list of integers separated by space
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let a=userInput[0];
+  console.log(a);
+
+  //end-here
+});
