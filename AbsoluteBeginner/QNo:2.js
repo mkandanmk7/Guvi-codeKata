05ef7fff2d477752df3ceee27e264df791b2b80b
Muthu <84056393+mkandanmk7@users.noreply.github.com>
2021-08-04 08:10:07 +0530

    First 3 multiples of the given number

---
+++ AbsoluteBeginner/QNo:2.js
@@ -0,0 +1,32 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+// let a=userInput[0];
+// let empty=[];
+// for (let i=1;i<=3;i++)
+// {
+//     empty.push(i*a);
+// }
+// console.log(empty.join(" "));
+ let num=userInput[0];
+ let mul=[];
+ for(let i=1;i<=3;i++){
+    mul.push(i*num);     
+ }
+ console.log(mul.join(" "))  // remove the [] give the elements
+
+  //end-here
+});
